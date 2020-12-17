cea58c675c0df8b29e9587db3461a3e3b4e97f93
Gajanan Nagare <gajanannagare16@gmail.com>
2020-12-17 22:51:30 +0530

    completed the filter component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
-import formatCurrency from '../utils';
+import formatCurrency from "../utils";
 
 class Products extends Component {
   render() {
     return (
       <div>
         <ul className="products">
-          {this.props.products.map((product) => (
-            <li key={product._id}>
-              <div className="product">
-                <a href={"#" + product._id}>
-                  <img src={product.image} alt={product.title} />
-                  <p>{product.title}</p>
-                </a>
-                <div className="product-price">
-                    <div>
-                        {formatCurrency(product.price)}
+          {this.props.products.length === 0
+            ? "No Product available for given size"
+            : this.props.products.map((product) => (
+                <li key={product._id}>
+                  <div className="product">
+                    <a href={"#" + product._id}>
+                      <img src={product.image} alt={product.title} />
+                      <p>{product.title}</p>
+                    </a>
+                    <div className="product-price">
+                      <div>{formatCurrency(product.price)}</div>
+                      <button className="button primary">Add To Cart</button>
                     </div>
-                    <button className="button primary">Add To Cart</button>
-                </div>
-              </div>
-            </li>
-          ))}
+                  </div>
+                </li>
+              ))}
         </ul>
       </div>
     );
